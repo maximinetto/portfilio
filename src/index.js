3895cc6f14e7088b2ad68cb7a71868536e0a2fcf
Maximiliano Minetto <maximinetto@gmail.com>
2021-07-10 22:18:38 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import 'normalize.css';
+import './styles/color-1.css';
+import './styles/index.css';
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  devtool: 'eval-cheap-module-source-map',
+};
